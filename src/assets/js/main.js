70c10e822a39f04d4c6c2a0b9a887cc84e16165d
Francois Szczepkowski <francois.szcz@hotmail.fr>
2021-08-31 20:28:36 +0200

    mise a jour index

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,18 @@
 "use strict";
+/*-------------Import GSAP-----------------*/
+
+// import  gsap  from "gsap";
+// import  ScrollTrigger  from "gsap/ScrollTrigger";
+// gsap.registerPlugin(ScrollTrigger);
+
+// import { gsap } from "gsap/dist/gsap";
+// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+// gsap.registerPlugin(ScrollTrigger);
+
+// const { gsap } = require("gsap/dist/gsap");
+// const { ScrollTrigger } = require("gsap/dist/ScrollTrigger");
+// gsap.registerPlugin(ScrollTrigger);
+
 /*----- DarkMode -----*/
 // let theme = document.body.getAttribute("data-theme");
 // if(theme == "dark"){
@@ -85,3 +99,87 @@ let heure   = new Date().getHours();
 let date = new Date().getFullYear(); 
 
 document.getElementById("year").innerHTML = date;
+
+
+
+/*----------Selecteur dans le html----------*/
+let btns = document.querySelectorAll('.btn');
+let sectionEls = document.querySelectorAll('.section__el');
+let section = document.querySelectorAll('.section');
+
+/*----------Annimation apparition au scroll sur index----------*/
+
+for(let i = 0 ; i < section.length ; i++){
+    if(i < 1){
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: section[i],
+          start: '0% 100%',
+          end: '50% 15%',
+          // toggleActions: 'play reverse',
+          //scrub: true,
+          // markers: true
+        }
+      });
+      tl.from(section[i],{
+        opacity: 0,
+        duration: 2,
+        ease: 'power2.out'
+      });
+    }else{
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: section[i],
+          start: '20% 80%',
+          end: '50% 15%',
+          // toggleActions: 'play reverse',
+          //scrub: true,
+          // markers: true
+        }
+      });
+      tl.from(section[i],{
+        opacity: 0,
+        y : 30,
+        duration: 2,
+        ease: 'power2.out'
+      });
+    }
+  };
+/*----------Annimation apparition au scroll sur Gallery----------*/
+
+for(let i = 0 ; i < sectionEls.length ; i++){
+    if(i < 5){
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: sectionEls[i],
+          start: '0% 100%',
+          end: '50% 15%',
+          // toggleActions: 'play reverse',
+          //scrub: true,
+          // markers: true
+        }
+      });
+      tl.from(sectionEls[i],{
+        opacity: 0,
+        duration: 2,
+        ease: 'power2.out'
+      });
+    }else{
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: sectionEls[i],
+          start: '20% 80%',
+          end: '50% 15%',
+          // toggleActions: 'play reverse',
+          //scrub: true,
+          // markers: true
+        }
+      });
+      tl.from(sectionEls[i],{
+        opacity: 0,
+        y : 30,
+        duration: 2,
+        ease: 'power2.out'
+      });
+    }
+  };
